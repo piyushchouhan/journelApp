7fe2663599863e99cb70c8b0b1e565359c30b9e1
Piyush Chauhan <60496644+piyushchouhan@users.noreply.github.com>
2024-11-30 15:38:03 +0530

    spring security added

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -1,26 +1,42 @@
 package net.engineeringdigest.journalApp.config;
 
-import org.springframework.boot.web.server.Http2;
+import net.engineeringdigest.journalApp.service.UserDetailsServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SpringSecurity extends WebSecurityConfiguration {
+public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private UserDetailsServiceImp userDetailsService;
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .httpBasic()
-                .and()
                 .authorizeRequests()
-                .antMatchers("/api/user").permitAll()
-                .antMatchers("/api/user/**").permitAll()
-                .antMatchers("/api/journal").permitAll()
-                .antMatchers("/api/journal/**").permitAll()
-                .anyRequest().authenticated()
+                .antMatchers("/api/journal/**","/api/user/**").authenticated()
+                .anyRequest().permitAll()
                 .and()
-                .csrf().disable();
+                .httpBasic();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
